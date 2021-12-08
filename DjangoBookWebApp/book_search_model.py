eee1b8b32fbc4e41340dfe5090f14edfbc5f5321
tsteternlieb <73751684+tsteternlieb@users.noreply.github.com>
2021-12-08 18:00:14 -0500

    fix

--- DjangoBookWebApp/book_search_model.py
+++ DjangoBookWebApp/book_search_model.py
@@ -174,8 +174,7 @@ class FinalWrapper():
     def __init__(self):
         #raise NameError(os.getcwd())
         self.__init_files()
-        corpus = gensim_api.load('text8')
-        self.w2v = model = Word2Vec(corpus)
+        self.w2v = Word2Vec.load('DjangoBookWebApp/research/w2v.model')
         self.stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn']
         self.vectorWrapper = VectorWrapper(self.int_to_weight,self.book_to_int,self.int_to_book)        
         self.search = SearchAlgo(self.vectorWrapper,self.w2v,self.transformer, .8, self.book_to_int)
